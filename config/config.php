efbc2e683717eac2c31d5b354332c63c26ec0c14
Enrique <mrstinky64@gmail.com>
2021-11-30 14:31:21 -0700

    primer commit

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+define('URL', 'http://localhost/SistemaAltelWeb/');
+
+define('DB', 'expenseapp');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'libs/database.php';
+require_once 'libs/messages.php';
+
+require_once 'libs/controller.php';
+require_once 'libs/view.php';
+require_once 'libs/model.php';
+require_once 'libs/app.php';
+
+
+require_once 'classes/session.php';
+require_once 'classes/sessionController.php';
+require_once 'classes/errors.php';
+require_once 'classes/success.php';
+
+
+require_once 'config/config.php';
+
+include_once 'models/usermodel.php';
+include_once 'models/expensesmodel.php';
+include_once "models/categoriesmodel.php";
+include_once "models/joinexpensescategoriesmodel.php";
+
+$app = new App();
+
+?>
