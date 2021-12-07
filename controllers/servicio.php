1315cf675f9a85fadc45a8dd3c77fc795573c4bd
Enrique <mrstinky64@gmail.com>
2021-12-07 14:06:46 -0700

    (commit de seguridad) formulario de editar servicio incompleto

--- controllers/servicio.php
+++ controllers/servicio.php
@@ -17,10 +17,53 @@ class Servicio extends SessionController{
         $this->view->render('servicio/index', ['user' => $this->user, 'servicios' => $this->model->getAll()]);
     }
     
-    function verServicio($params)
-    {
+    function verServicio($params){
         $id = $params[0];
         $this->view->render('servicio/verServicio', ['user' => $this->user, 'servicio' => $this->model->get($id)]);
+    }
+
+    function editarServicio(){
+
+        if($this->existPOST(['id'])){
+            $id = $this->getPost('id');
+
+            $this->view->render('servicio/editarServicio', ['user' => $this->user, 'servicio' => $this->model->get($id)]);
+        }
+    }
+
+    function guardarServicio(){
+
+        if($this->existPOST(['id', 'cuenta', 'fecha_alta', 'nombre', 'status', 'problema', 'fecha_realizar', 'hora_realizar', 'capturo_alta', 'costo', 'tecnico', 'capturo_baja', 'observacion_problema', 'direccion', 'colonia', 'entre_calles', 'observacion_servicio', 'status_recorrido', 'seguimiento', 'folio', 'no_reagendaciones'])){
+            $id = $this->getPost('id');
+            $cuenta = $this->getPost('cuenta');
+            $fecha_alta = $this->getPost('fecha_alta');
+            $nombre = $this->getPost('nombre');
+            $status = $this->getPost('status');
+            $problema = $this->getPost('problema');
+            $fecha_realizar = $this->getPost('fecha_realizar');
+            $hora_realizar = $this->getPost('hora_realizar');
+            $capturo_alta = $this->getPost('capturo_alta');
+            $costo = $this->getPost('costo');
+            $tecnico = $this->getPost('tecnico');
+            $capturo_baja = $this->getPost('capturo_baja');
+            $observacion_problema = $this->getPost('observacion_problema');
+            $direccion = $this->getPost('direccion');
+            $colonia = $this->getPost('colonia');
+            $entre_calles = $this->getPost('entre_calles');
+            $observacion_servicio = $this->getPost('observacion_servicio');
+            $status_recorrido = $this->getPost('status_recorrido');
+            $seguimiento = $this->getPost('seguimiento');
+            $folio = $this->getPost('folio');
+            $no_reagendaciones = $this->getPost('no_reagendaciones');
+
+            $this->model->setId($id);
+            $this->model->setCuenta($cuenta);
+            $this->model->setFecha_alta($fecha_alta);
+        
+
+            $this->view->render('servicio/editarServicio', ['user' => $this->user, 'servicio' => $this->model->get($id)]);
+        }
+
     }
     
 }
--- views/servicio/editarServicio.php
+++ views/servicio/editarServicio.php
@@ -13,28 +13,28 @@
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Ver Servicio</h4>
+                            <h4 class="card-title"> Editar Servicio</h4>
                         </div>
                         <div class="card-body">
-                            <form action="../editarServicio" method="POST">
+                            <form action="../guardarServicio">
                                 <input type="hidden" name="id" value=<?php echo $servicio->getId()?>>
                                 <div class="row">
                                     <div class="col-md-4 pr-1">
                                         <div class="form-group">
                                             <label>Cuenta</label>
-                                            <input type="text" class="form-control" style="color:black" disabled value=<?php echo $servicio->getCuenta()?>>
+                                            <input type="text" name="cuenta" class="form-control" style="color:black"  value=<?php echo $servicio->getCuenta()?>>
                                         </div>
                                     </div>
                                     <div class="col-md-4 px-1">
                                         <div class="form-group">
                                             <label>Fecha de alta</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getFecha_alta()?>>
+                                            <input type="text" name="fecha_alta" class="form-control" style="color:black"  value=<?php echo $servicio->getFecha_alta()?>>
                                         </div>
                                     </div>
                                     <div class="col-md-4 pl-1">
                                         <div class="form-group">
                                             <label>Nombre</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getNombre()?>>
+                                            <input type="text" name="nombre" class="form-control" style="color:black"  value=<?php echo $servicio->getNombre()?>>
                                         </div>
                                     </div>
                                 </div>
@@ -42,7 +42,7 @@
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Status</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getStatus()?>>
+                                            <input type="text" name="status" class="form-control" style="color:black"  value=<?php echo $servicio->getStatus()?>>
                                         </div>
                                     </div>
                                 </div>
@@ -50,7 +50,7 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>Problema</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getProblema()?>>
+                                            <input type="text" name="problema" class="form-control" style="color:black"  value=<?php echo $servicio->getProblema()?>>
                                         </div>
                                     </div>
                                 </div>
@@ -58,19 +58,19 @@
                                     <div class="col-md-4 pr-1">
                                         <div class="form-group">
                                             <label>fecha a realizar</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getFecha_realizar()?>>
+                                            <input type="text" name="fecha_realizar" class="form-control" style="color:black"  value=<?php echo $servicio->getFecha_realizar()?>>
                                         </div>
                                     </div>
                                     <div class="col-md-4 px-1">
                                         <div class="form-group">
                                             <label>hora</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getHora_realizar()?>>
+                                            <input type="text" name="hora_realizar" class="form-control" style="color:black"  value=<?php echo $servicio->getHora_realizar()?>>
                                         </div>
                                     </div>
                                     <div class="col-md-4 pl-1">
                                         <div class="form-group">
                                             <label>capturo alta</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getCapturo_alta()?>>
+                                            <input type="text" name="capturo_alta" class="form-control" style="color:black"  value=<?php echo $servicio->getCapturo_alta()?>>
                                         </div>
                                     </div>
                                 </div>
@@ -78,19 +78,19 @@
                                     <div class="col-md-4 pr-1">
                                         <div class="form-group">
                                             <label>costo</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getCosto()?>>
+                                            <input type="text" name="costo" class="form-control" style="color:black"  value=<?php echo $servicio->getCosto()?>>
                                         </div>
                                     </div>
                                     <div class="col-md-4 px-1">
                                         <div class="form-group">
                                             <label>tecnico</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getTecnico()?>>
+                                            <input type="text" name="tecnico" class="form-control" style="color:black"  value=<?php echo $servicio->getTecnico()?>>
                                         </div>
                                     </div>
                                     <div class="col-md-4 pl-1">
                                         <div class="form-group">
                                             <label>capturo baja</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getCapturo_baja()?>>
+                                            <input type="text" name="capturo_baja" class="form-control" style="color:black"  value=<?php echo $servicio->getCapturo_baja()?>>
                                         </div>
                                     </div>
                                 </div>
@@ -98,7 +98,7 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>observacion del Problema</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getObservacion_problema()?>>
+                                            <input type="text" name="observacion_problema" class="form-control" style="color:black"  value=<?php echo $servicio->getObservacion_problema()?>>
                                         </div>
                                     </div>
                                 </div>
@@ -106,13 +106,13 @@
                                     <div class="col-md-6 pr-1">
                                         <div class="form-group">
                                             <label>direccion</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getDireccion()?>>
+                                            <input type="text" name="direccion" class="form-control" style="color:black"  value=<?php echo $servicio->getDireccion()?>>
                                         </div>
                                     </div>
                                     <div class="col-md-6 pl-1">
                                         <div class="form-group">
                                             <label>colonia</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getColonia()?>>
+                                            <input type="text" name="colonia" class="form-control" style="color:black"  value=<?php echo $servicio->getColonia()?>>
                                         </div>
                                     </div>
                                 </div>
@@ -120,7 +120,7 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>entre calles</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getEntre_calles()?>>
+                                            <input type="text" name="entre_calles" class="form-control" style="color:black"  value=<?php echo $servicio->getEntre_calles()?>>
                                         </div>
                                     </div>
                                 </div>
@@ -128,7 +128,7 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>observacion del servicio</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getObservacion_servicio()?>>
+                                            <input type="text" name="observacion_servicio" class="form-control" style="color:black"  value=<?php echo $servicio->getObservacion_servicio()?>>
                                         </div>
                                     </div>
                                 </div>
@@ -136,13 +136,13 @@
                                     <div class="col-md-6 pr-1">
                                         <div class="form-group">
                                             <label>status del recorrido</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getStatus_recorrido()?>>
+                                            <input type="text" name="status_recorrido" class="form-control" style="color:black"  value=<?php echo $servicio->getStatus_recorrido()?>>
                                         </div>
                                     </div>
                                     <div class="col-md-6 pl-1">
                                         <div class="form-group">
                                             <label>seguimiento</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getSeguimiento()?>>
+                                            <input type="text" name="seguimiento" class="form-control" style="color:black"  value=<?php echo $servicio->getSeguimiento()?>>
                                         </div>
                                     </div>
                                 </div> 
@@ -150,17 +150,17 @@
                                     <div class="col-md-6 pr-1">
                                         <div class="form-group">
                                             <label>folio</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getFolio()?>>
+                                            <input type="text" name="folio" class="form-control" style="color:black"  value=<?php echo $servicio->getFolio()?>>
                                         </div>
                                     </div>
                                     <div class="col-md-2 px-1">
                                         <div class="form-group">
                                             <label>reagendaciones</label>
-                                            <input type="text" class="form-control" style="color:black" disabled="" value=<?php echo $servicio->getNo_reagendaciones()?>>
+                                            <input type="text" name="no_reagendaciones" class="form-control" style="color:black"  value=<?php echo $servicio->getNo_reagendaciones()?>>
                                         </div>
                                     </div>
                                 </div> 
-                                <button type="submit" class="btn btn-info btn-fill pull-right">Editar</button>
+                                <button type="submit" class="btn btn-success btn-fill pull-right">Guardar</button>
                                 <div class="clearfix"></div>
                             </form>
                         </div>
